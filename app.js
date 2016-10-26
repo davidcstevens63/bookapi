d5fcf62edc518e88044f41a15e6e965e333b2dde
mainstreetengineering <mainstreetengineering@gmail.com>
2016-10-26 11:27:24 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// JavaScript source code
+
+var express = require('express'),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+bookRouter = require('./Routes/bookRoutes')(Book);
+
+app.use('/api/Books', bookRouter);
+//app.use('/api/author', authorRouter);
+
+app.get('/', function (req, res) {
+    res.send(('Welocome to our api '))
+});
+
+app.listen(port, function () {
+    console.log("Gulp is running on port: " + port);
+});
