6ad5788e4ec9de5c53465956af6d8b23f8b3762d
davidcstevens63 <davidcstevens@comcast.net>
2016-10-27 08:38:07 -0700

    making unit test fail

--- Tests/bookControllerTests.js
+++ Tests/bookControllerTests.js
@@ -22,7 +22,7 @@ describe('Book Controller Tests:', function(){
             
             bookController.post(req,res);
 
-            res.status.calledWith(400).should.equal(true, 'Bad Status ' + res.status.args[0][0]);
+            res.status.calledWith(400).should.equal(false, 'Bad Status ' + res.status.args[0][0]);
             res.send.calledWith('Title is Required').should.equal(true);
         })
     })
